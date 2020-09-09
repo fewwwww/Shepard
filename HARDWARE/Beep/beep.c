37a4687b595ca3c3e97bd92d1dd1fcf190739784
Suning Yao <41555918+fewwwww@users.noreply.github.com>
2020-09-09 19:28:26 +0800

    Update beep.c

--- HARDWARE/Beep/beep.c
+++ HARDWARE/Beep/beep.c
@@ -8,10 +8,10 @@ void Beep_Init(void)
 	GPIO_InitTypeDef GPIO_InitStructure;    
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);  
 	GPIO_InitStructure.GPIO_Pin  = GPIO_Pin_3;//PA3
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; //设置为推挽输出
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
  	GPIO_Init(GPIOA, &GPIO_InitStructure);	
-	GPIO_SetBits(GPIOA,GPIO_Pin_3);				 //PA3 输出高
+	GPIO_SetBits(GPIOA,GPIO_Pin_3);				 
 
 }
  
